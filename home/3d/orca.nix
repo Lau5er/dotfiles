e6c902073bf96ef31c59fc40272e78cbb8fd84e4
Lau5er <lau5er@icloud.com>
2026-05-21 01:14:29 +0200

    home(lauser): add snapmaker-orca wrapper and desktop via home.file

--- home/3d/orca.nix
+++ home/3d/orca.nix
@@ -16,4 +16,28 @@
     })
 
   ];
+
+  # Create a user-local wrapper that forces a usable locale for snapmaker-orca
+  home.file = {
+    ".local/bin/snapmaker-orca".text = ''
+#!/bin/sh
+export LC_ALL=C.utf8
+export LANG=C.utf8
+exec /run/current-system/sw/bin/snapmaker-orca "$@"
+'';
+    ".local/bin/snapmaker-orca".mode = "0755";
+
+    ".local/share/applications/snapmaker-orca.desktop".text = ''
+[Desktop Entry]
+Name=Snapmaker Orca
+GenericName=3D Slicer
+Comment=Snapmaker Orca slicer (starts with C.utf8 locale)
+Exec=$HOME/.local/bin/snapmaker-orca %U
+Terminal=false
+Type=Application
+Categories=Graphics;Science;
+Icon=snapmaker-orca
+StartupWMClass=OrcaSlicer
+'';
+  };
 }
